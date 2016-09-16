d008a6bf140d457e2cee2888fab7c338b7260c31
B4Bentley <B4Bentley>
2016-09-16 19:03:45 -0400

    initial prompt of user has been created

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -18,3 +18,28 @@ startConnection.connect(function(err){
     console.log('Connection to Bamazon has been established');
 });
 
+
+startConnection.query('SELECT itemID, productName, price FROM Products',function(err,rows){
+    if(err) {
+        throw err;
+    }
+    console.log('Query to view itemID, productName, and price:\n');
+    console.log(rows);
+
+    prompt.start();
+
+    console.log("Please enter the ID of the product they would like to buy.");
+    console.log("Please enter how many units of the product you would like to buy.")
+
+    prompt.get(['itemID', 'stockQuantity'], function (err, result) {
+
+        var ID = result.itemID;
+        var quantity = result.stockQuantity;
+        console.log('the Id for the product you selected is: ' + ID);
+        console.log('the quantity for the product you selected is: ' + quantity);
+
+
+    });
+});
+
+startConnection.end();
