2e6c9f9ff0fb76f9a6bc2f6e2c1c79bba2520ae8
B4Bentley <B4Bentley>
2016-09-17 11:23:03 -0400

    function added to query the database based on user input

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var prompt = require('prompt');
+var inquirer = require('inquirer');
 
 var startConnection = mysql.createConnection({
     host: "localhost",
@@ -19,27 +20,77 @@ startConnection.connect(function(err){
 });
 
 
-startConnection.query('SELECT itemID, productName, price FROM Products',function(err,rows){
-    if(err) {
-        throw err;
+startConnection.query('select * from Products', function(err,rows){
+
+//initial print each product 
+    for(var i =0; i<rows.length; i++){
+        console.log(rows[i].itemId + "\t" + rows[i].productName + "\t\t" + rows[i].price);
     }
-    console.log('Query to view itemID, productName, and price:\n');
-    console.log(rows);
 
-    prompt.start();
+//call the userPromptProchase function
+    userPromptPurchase();
+});
 
-    console.log("Please enter the ID of the product they would like to buy.");
-    console.log("Please enter how many units of the product you would like to buy.")
+//userPromptPurchase function 
+function userPromptPurchase(){
 
-    prompt.get(['itemID', 'stockQuantity'], function (err, result) {
+//call the inquirer npm package to prompt the user
+ inquirer.prompt([
+//will ask both question as a object array [{},{}]
+// first question
+        {
+            name: "id",
+            type: "input",
+            message: "Please provide the productID for the item you would like to Purchase:",
+            validate: function(value) {
 
-        var ID = result.itemID;
-        var quantity = result.stockQuantity;
-        console.log('the Id for the product you selected is: ' + ID);
-        console.log('the quantity for the product you selected is: ' + quantity);
+                //Validates answer
+                if (isNaN(value) === false) {
+                    return true;
+                } else {
+                    console.log("\nPlease provide the productID for the item you would like to Purchase:\n");
+                    return false;
+                }
+            }
+        },
+
+//second question
+        {
+            name: "qty",
+            type: "input",
+            message: "How many whould you like to purchase:",
+            validate: function(value) {
+                //validates answer
+                if (isNaN(value) === false) {
+                    return true;
+                } else {
+                    console.log("\nPlease enter a valid quantity\n");
+                    return false;
+                }
+            }
+        }
+    ]).then(function(answer) {
+
+            //Queries the database for the desired id and qty
+        startConnection.query("SELECT * FROM Products WHERE ?", [{itemID: answer.id}], function(err, data) {
+                if (err) throw err;
+                //console check to see if the data was being received from query result
+                //console.log(data);
+
+                if(data[0].stockQuantity < answer.qty){
+                    //check to see if the check works
+                    //console.log("made it the if statement true");
+                    console.log("Sorry not enough qty in stock \n");
+                    console.log("Please select another product id or lower quantity");
+                    userPromptPurchase();
+                }else{
+                    //check if you made it past the check
+                    console.log('success');
+                }
+        });
 
 
     });
-});
+};
 
-startConnection.end();
+// startConnection.end();
